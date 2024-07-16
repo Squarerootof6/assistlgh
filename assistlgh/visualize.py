2ede6ef58d62886ac1b13d9e59d9c46fbdc7c5cd
Genghao Liu <squarerootof6@outlook.com>
2024-07-16 20:50:02 +0800

    add more cmap

--- assistlgh/visualize.py
+++ assistlgh/visualize.py
@@ -27,3 +27,16 @@ def auto_fontsize(figsize=4.8,fac=3):
         'xtick.labelsize': font_size,
         'ytick.labelsize': font_size,
     })
+    
+def more_cmap(color_bin = 5):
+    from matplotlib.colors import LinearSegmentedColormap
+    if color_bin ==2:
+        colors = [(0,"#EE82EE"),(1,"#00D1FF")]
+    elif color_bin==3:
+        colors = [(0,'#1A2A6C'),(0.5,"#B21F1F"),(1,"#FDBB2D")]
+    elif color_bin ==4:
+        colors = [(0,"#0D375F"),(0.3155,"#BAD3E6"),(0.6565,"#F67579"),(1,"#C50020")]
+    elif color_bin==5:
+        colors = [(0,'#1F0654'),(0.2682,'#72197D'),(0.5063,'#AC1E1E'),(0.7651,'#E99227'),(1,'#FFEFBA')]
+
+    return LinearSegmentedColormap.from_list('mycmap', colors)
